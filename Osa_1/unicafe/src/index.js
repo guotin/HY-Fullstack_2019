66d1e9a2edc086ae08f53ad2dde643aee0babe2d
guotin <eerokauhanen@outlook.com>
2019-05-20 15:07:51 +0300

    sisennyskorjaus

--- Osa_1/unicafe/src/index.js
+++ Osa_1/unicafe/src/index.js
@@ -4,64 +4,64 @@ import ReactDOM from 'react-dom'
 const Statistics = (props) => {
   const good=props.good
   const neutral=props.neutral
-	const bad=props.bad
-	const total=good+neutral+bad
-	const average=(good-bad) / total
-	const positive=good/total * 100
+  const bad=props.bad
+  const total=good+neutral+bad
+  const average=(good-bad) / total
+  const positive=good/total * 100
 
-	if (total > 0) {
-		return (
-			<div>
-				<table>
-					<Statistic text="hyvä" value={good} />
-					<Statistic text="neutraali" value={neutral} />
-					<Statistic text="paha" value={bad} />
-					<Statistic text="yhteensä" value={total} />
-					<Statistic text="keskiarvo" value={average} />
-					<Statistic text="positiivisia" value={positive} mark="%" />
-				</table>
-			</div>
-		)
-	}
-	return (
-		<div>
-			<p>Ei yhtään palautetta annettu</p>
-		</div>
-	) 
+  if (total > 0) {
+    return (
+      <div>
+        <table>
+          <Statistic text="hyvä" value={good} />
+          <Statistic text="neutraali" value={neutral} />
+          <Statistic text="paha" value={bad} />
+          <Statistic text="yhteensä" value={total} />
+          <Statistic text="keskiarvo" value={average} />
+          <Statistic text="positiivisia" value={positive} mark="%" />
+        </table>
+     </div>
+    )
+  }
+  return (
+    <div>
+      <p>Ei yhtään palautetta annettu</p>
+    </div>
+  )
 }
 
 const Statistic = (props) => {
-	const text=props.text
-	const value=props.value
-	const mark=props.mark
+  const text=props.text
+  const value=props.value
+  const mark=props.mark
 
-	return (
-		<tbody>
-			<tr>
-				<td>{text} {value} {mark}</td>
-			</tr>
-		</tbody>
-	)
+  return (
+    <tbody>
+      <tr>
+        <td>{text} {value} {mark}</td>
+      </tr>
+    </tbody>
+  )
 }
 
 const Button = ({ handleClick, text}) => (
-	<button onClick={handleClick}>
-		{text}
-	</button>
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-	const [bad, setBad] = useState(0)
+  const [bad, setBad] = useState(0)
 	
   return (
     <div>
       <h1>Anna palautetta</h1>
       <Button handleClick={() => setGood(good + 1)} text="hyvä" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutraali" />
-			<Button handleClick={() => setBad(bad + 1)} text="huono" />
-			<h1>statistiikka</h1>
+      <Button handleClick={() => setBad(bad + 1)} text="huono" />
+      <h1>statistiikka</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
